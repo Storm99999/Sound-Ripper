229278ed5b93fd428780d96075377e23fb03c415
Storm <87811650+Storm99999@users.noreply.github.com>
2021-09-24 13:15:51 +0200

    Add files via upload

---
+++ H/Format.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H
+{
+    class Format
+    {
+        public static bool GetDefaultFormat()
+        {
+            return true;
+        }
+    }
+}
