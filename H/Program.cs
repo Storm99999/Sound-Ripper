725e92275769c695884e833b8b0d294c4c43a3ea
Storm <87811650+Storm99999@users.noreply.github.com>
2021-09-24 13:18:02 +0200

    Add files via upload

---
+++ H/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VideoLibrary;
+
+namespace H
+{
+    class Program
+    {
+        bool format = Format.GetDefaultFormat();
+        static void Main(string[] args)
+        {
+            WriteLine("Enter The Url: ");
+            var url = Console.ReadLine();
+            Download(url);
+            Console.ReadLine();
+        }
+
+        public static async void Download(string url)
+        {
+            var yt = YouTube.Default;
+            var vud = await yt.GetVideoAsync(url);
+            File.WriteAllBytes(Environment.CurrentDirectory + $"\\{vud.FullName}.wav", vud.GetBytes());
+        }
+        public static void WriteLine(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("[");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write("H++");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("] " + message + "\n");
+        }
+    }
+}
